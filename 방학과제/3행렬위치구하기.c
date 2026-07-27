5ba99223f967d02351c28e1535e87ae51e4c32ad
carhope <26_61@bssm.hs.kr>
2026-07-27 15:53:34 +0900

    3번 해결 c언어로 배열 내 약점 극복

--- 방학과제/3행렬위치구하기.c
+++ 방학과제/3행렬위치구하기.c
@@ -1,10 +1,24 @@
 /* 행 r, 열 c, 찾아야할 번호 k 입력
 k가 있는 행과 열 출력
 2차원 배열사용
-각각 들어갈 번호 저장 num*/
+각각 들어갈 번호 저장 curr*/
 #include <stdio.h>
 int main(){
     int r,c,k,num;
     scanf("%d %d %d",&r, &c,&k);
-    
+    int curr= 1;
+    int arr[r][c];
+    for (int i=0;i<r;i++){
+        for (int j=0;j<c;j++){
+            arr[i][j]=curr;
+            curr++;
+        }
+    }
+    for (int i=0;i<r;i++){
+        for (int j=0;j<c;j++){
+            if (arr[i][j]==k){
+                printf("%d %d",i+1,j+1);
+            }
+        }
+    }
 }
