cb3a4933dfc4eb2b504554c440737b4872d497b4
carhope <26_61@bssm.hs.kr>
2026-08-10 00:00:18 +0900

    4번시작

---
+++ 방학과제/2차시/4passpeople.c
@@ -0,0 +1,11 @@
+/*기준 점수 int p
+p입력 받기
+n개의 이름과 점수 입력받기 한줄에 이름과 점수 한개씩 n개임
+
+기준을 넘는 사람의 수 int passpeople = 0
+이름을 저장하는 배열 char name[n][100];
+점수를 저장하는 배열 int score[n]
+
+출력 
+printf("%d %s",passpeople,
+*/
